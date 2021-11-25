e53178c7d977a382d08f696e1c072f3ff297b4dd
sebastian <jotapunk83@gmail.com>
2021-11-25 15:14:04 -0500

    Primera version

---
+++ app/src/main/java/com/misiontic/view/ui/activities/SplashScreenActivity.kt
@@ -0,0 +1,35 @@
+package com.misiontic.view.ui.activities
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import com.misiontic.R
+import com.misiontic.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        val animation = AnimationUtils.loadAnimation(this, R.anim.animation)
+        binding.ivSplashScreen.startAnimation(animation)
+        val intent = Intent(this,MainActivity::class.java)
+        animation.setAnimationListener(object: Animation.AnimationListener{
+            override fun onAnimationStart(animation: Animation?) {
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                startActivity(intent)
+                finish()
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+            }
+        })
+    }
+}
